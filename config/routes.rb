d7a7efe96752228758f5e4c500af81441dd640a2
Nathan Lowrie <nate@finelineautomation.com>
2013-09-02 07:21:51 -0400

    Got rid of the markdown route now that we are not longer using it.
    Signed-off-by: Nathan Lowrie <nate@finelineautomation.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 #Rails.application
 Spree::Core::Engine.routes.draw do
   namespace :admin do
-    post "/markdown/preview" => "markdown#preview", :as => :markdown_preview
     resources :uploads
   end
 end
