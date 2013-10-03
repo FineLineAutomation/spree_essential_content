7eee1be04a164edc6b8ac9726e93811703a2f0ae
Nathan Lowrie <nate@finelineautomation.com>
2013-10-03 07:08:12 -0400

    Updated gem to be compatible with spree 1.3.
    Signed-off-by: Nathan Lowrie <nate@finelineautomation.com>

--- app/models/spree/upload.rb
+++ app/models/spree/upload.rb
@@ -1,21 +1,21 @@
 class Spree::Upload < ::Spree::Asset
-  
+
   attr_accessible :attachment, :alt
-  
-  default_scope where(:type => "Upload") if table_exists?
-  
+
+  default_scope where(:type => "Spree::Upload") if table_exists?
+
   validate :no_attachement_errors
-  
+
   has_attached_file :attachment,
     :styles        => Proc.new{ |clip| clip.instance.attachment_sizes },
     :default_style => :medium,
     :url           => "/spree/uploads/:id/:style/:basename.:extension",
     :path          => ":rails_root/public/spree/uploads/:id/:style/:basename.:extension"
-  
+
   def image_content?
     attachment_content_type.match(/\/(jpeg|png|gif|tiff|x-photoshop)/)
   end
-     
+
   def attachment_sizes
     if image_content?
       { :mini => '48x48>', :small => '150x150>', :medium => '420x300>', :large => '800x500>' }
@@ -23,7 +23,7 @@ class Spree::Upload < ::Spree::Asset
       {}
     end
   end
-  
+
   def no_attachement_errors
     if attachment_file_name.blank? || !attachment.errors.empty?
       # uncomment this to get rid of the less-than-useful interrim messages
