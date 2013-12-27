e46c18a3f76c9ea89112f8014905efc5ea2efa7d
Nathan Lowrie <nate@finelineautomation.com>
2013-12-27 12:24:31 -0500

    Got rid of the helper
    Since we are stripping out the engine functionality we no longer need it.
    
    Signed-off-by: Nathan Lowrie <nate@finelineautomation.com>

--- spec/helpers_spec_helper.rb
+++
@@ -1,6 +0,0 @@
-# not sure why setting the request_uri doesn't also update @fullpath
-def visit(path="/")
-  @request.request_uri = path
-  @request.instance_variable_set "@fullpath", path
-  @request
-end
