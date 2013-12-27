21627fd53ef7c1578be06df24a3712602675bf37
Nathan Lowrie <nate@finelineautomation.com>
2013-12-27 12:21:42 -0500

    Modify spec helper to be more in line with the current engine format.
    Signed-off-by: Nathan Lowrie <nate@finelineautomation.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,29 +1,48 @@
+# Run Coverage report
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'spec/dummy'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Mailers', 'app/mailers'
+  add_group 'Models', 'app/models'
+  add_group 'Views', 'app/views'
+  add_group 'Libraries', 'lib'
+end
+
 # Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
 
 require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 
 require 'rspec/rails'
+require 'database_cleaner'
 require 'ffaker'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
-require 'database_cleaner'
+# Requires factories defined in spree_core
+require 'spree/testing_support/factories'
+require 'spree/testing_support/controller_requests'
+require 'spree/testing_support/authorization_helpers'
+require 'spree/testing_support/url_helpers'
 
-require 'spree/core/testing_support/factories'
-require 'spree/core/testing_support/controller_requests'
-require 'spree/core/testing_support/authorization_helpers'
-require 'spree/core/testing_support/preferences'
-require 'spree/core/testing_support/flash'
-
-require 'spree/core/url_helpers'
-require 'capybara/rspec'
-require 'capybara/rails'
-require 'paperclip/matchers'
+# Requires factories defined in lib/spree_essential_content/factories.rb
+require 'spree_essential_content/factories'
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+  # == URL Helpers
+  #
+  # Allows access to Spree's routes in specs:
+  #
+  # visit spree.admin_path
+  # current_path.should eql(spree.products_path)
+  config.include Spree::TestingSupport::UrlHelpers
+
   # == Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -32,103 +51,32 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
   config.mock_with :rspec
+  config.color = true
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
+  # Capybara javascript drivers require transactional fixtures set to false, and we use DatabaseCleaner
+  # to cleanup after each test instead.  Without transactional fixtures set to false the records created
+  # to setup a test will be unavailable to the browser, which runs under a seperate server instance.
   config.use_transactional_fixtures = false
 
-  config.before(:each) do
-    if example.metadata[:js]
-      DatabaseCleaner.strategy = :truncation, { :except => ['spree_countries', 'spree_zones', 'spree_zone_members', 'spree_states', 'spree_roles'] }
-    else
-      DatabaseCleaner.strategy = :transaction
-    end
+  # Ensure Suite is set to use transactions for speed.
+  config.before :suite do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
   end
 
-  config.before(:each) do
+  # Before each spec check if it is a Javascript test and switch between using database transactions or not where necessary.
+  config.before :each do
+    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
     DatabaseCleaner.start
-    reset_spree_preferences
   end
 
-  config.after(:each) do
+  # After each spec clean the database.
+  config.after :each do
     DatabaseCleaner.clean
   end
 
-  config.include FactoryGirl::Syntax::Methods
-  config.include Spree::Core::UrlHelpers
-  config.include Spree::Core::TestingSupport::ControllerRequests
-  config.include Spree::Core::TestingSupport::Preferences
-  config.include Spree::Core::TestingSupport::Flash
-
-  config.include Paperclip::Shoulda::Matchers
-end
-
-def fill_in_js(selector, value)
-  page.execute_script %Q{$('#{selector}').val('#{value}').keydown()}
-end
-
-
-def spree
-  Spree::Core::Engine.routes.url_helpers
-end
-
-def request
-  @request
-end
-
-# Stub authorization for all admin controllers
-def stub_authorization!
-  subclasses = Spree::Admin::BaseController.subclasses + Spree::Admin::ResourceController.subclasses
-  subclasses.each do |klass|
-    klass.any_instance.stub(:authorize!).and_return(true)
-  end
-end
-
-# An assertion for ensuring content has made it to the page.
-#
-#    assert_seen "Site Title"
-#    assert_seen "Peanut Butter Jelly Time", :within => ".post-title h1"
-#
-def assert_seen(text, opts={})
-  msg = "Should see `#{text}`"
-  if opts[:within]
-    within(opts[:within]) do
-      expect(has_content?(text)).to eq(true)
-    end
-  else
-    expect(has_content?(text)).to eq(true)
-  end
-end
-
-# Asserts the proper flash message
-#
-#    assert_flash :notice, "Post was successfully saved!"
-#    assert_flash :error, "Oh No, bad things happened!"
-#
-def assert_flash(key, text)
-  within(".flash.#{key}") do
-    assert_seen(text)
-  end
-end
-
-# Asserts the proper browser title
-#
-#    assert_title "My Site - Is super cool"
-#
-def assert_title(title)
-  assert_seen title, :within => "head title"
-end
-
-# Asserts meta tags have proper content
-#
-#    assert_meta :description, "So let me tell you about this one time..."
-#    assert_meta :keywords, "seo, is, fun, jk."
-#
-def assert_meta(tag, text)
-  tag = find(:xpath, "//head/meta[@name='#{tag.to_s}']")
-  expect(text).to eq(tag.native.attribute("content"))
+  config.fail_fast = ENV['FAIL_FAST'] || false
 end
