9020782f2f889e3053be43fd8043fa1945f9ae54
Nathan Lowrie <nate@finelineautomation.com>
2014-02-10 18:55:50 -0500

    Translations and styling on admin pages for posts and uploads
    Moved to Spree 2.0 translations. Translations in this extension are a mess at the moment.
    
    Signed-off-by: Nathan Lowrie <nate@finelineautomation.com>

--- app/controllers/spree/admin/uploads_controller.rb
+++ app/controllers/spree/admin/uploads_controller.rb
@@ -1,5 +1,18 @@
-class Spree::Admin::UploadsController < Spree::Admin::ResourceController
-  def collection
-    super.order(:attachment_updated_at)
+module Spree
+  module Admin
+    class UploadsController < ResourceController
+      private
+
+        def find_resource
+          @upload ||= ::Spree::Upload.find_by_id(params[:id])
+        end
+
+        def collection
+          params[:q] ||= {}
+          params[:q][:s] ||= "attachment_updated_at asc"
+          @search = Spree::Upload.search(params[:q])
+          @collection = @search.result.page(params[:page]).per(Spree::Config[:orders_per_page])
+        end
+    end
   end
 end
