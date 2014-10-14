1ac3ac5f9ec929dced431f755f699ab90b4dfb4d
Nathan Lowrie <nate@finelineautomation.com>
2014-10-14 00:46:39 -0400

    Get extension working for 2.2
    Moved asset locations into vendor. Updated install generator to point at appropriate locations.
    
    Signed-off-by: Nathan Lowrie <nate@finelineautomation.com>

--- lib/generators/spree_essential_content/install/install_generator.rb
+++ lib/generators/spree_essential_content/install/install_generator.rb
@@ -5,13 +5,13 @@ module SpreeEssentialContent
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_essential_content\n//= require store/spree_frontend"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_essential_content\n//= require admin/spree_backend"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require /spree/frontend/spree_essential_content\n"
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_essential_content\n"
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_essential_content\n *= require store/spree_frontend", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_essential_content\n *= require store/spree_backend", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_essential_content\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_essential_content\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
