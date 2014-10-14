2bd5fa448b8a7025e80d421681b21cd3bc5ae556
Nathan Lowrie <nate@finelineautomation.com>
2014-10-13 22:29:41 -0400

    Fix reg expression to use spree enabled attributes
    Signed-off-by: Nathan Lowrie <nate@finelineautomation.com>

--- db/migrate/20120308121120_create_default_blog.rb
+++ db/migrate/20120308121120_create_default_blog.rb
@@ -1,7 +1,7 @@
 class CreateDefaultBlog < ActiveRecord::Migration
   def self.up
-    @blog = Spree::Blog.create(:name => "Blog", :permalink => "blog")
-    Spree::Post.update_all(:blog_id => @blog.id)
+    @blog = Spree::Blog.create(name: "Blog", permalink: "blog")
+    Spree::Post.update_all(blog_id: @blog.id)
   end
 
   def self.down
