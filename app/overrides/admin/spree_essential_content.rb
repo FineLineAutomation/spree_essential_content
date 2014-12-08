c81d9f5114a1bbcbe4998d732df27bd5e7c59d94
Nathan Lowrie <nate@finelineautomation.com>
2014-12-08 14:22:05 -0500

    Fixed admin menu partials
    The menu got subbed out to a new partial. Needed to update the reference and update the translations.

--- app/overrides/admin/spree_essential_content.rb
+++ app/overrides/admin/spree_essential_content.rb
@@ -1,5 +1,5 @@
 # Adds the Content button to the admin tabs
-Deface::Override.new(virtual_path:  "spree/layouts/admin",
+Deface::Override.new(virtual_path:  "spree/admin/shared/_menu",
                      name:          "spree_essential_admin_tabs",
                      insert_bottom: "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
                      partial:       "spree/admin/shared/contents_tab",
