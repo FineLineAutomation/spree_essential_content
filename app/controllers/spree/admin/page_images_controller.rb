4c05d222d6f486e0c5a16ccfc581708424d748a4
Nathan Lowrie <nate@finelineautomation.com>
2015-07-04 09:15:09 -0400

    Fixed the page image positioning

--- app/controllers/spree/admin/page_images_controller.rb
+++ app/controllers/spree/admin/page_images_controller.rb
@@ -5,15 +5,6 @@ class Spree::Admin::PageImagesController < Spree::Admin::ResourceController
   update.before :set_viewable
   destroy.before :destroy_before
 
-  def update_positions
-    params[:positions].each do |id, index|
-      Spree::PageImage.update_all(['position=?', index], ['id=?', id])
-    end
-    respond_to do |format|
-      format.js  { render text: 'Ok' }
-    end
-  end
-
   private
 
   def location_after_save
