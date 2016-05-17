a4913c6162e95c80db4a7f54261a1aa39c4408f3
Nathan Lowrie <nate@finelineautomation.com>
2016-05-17 07:12:45 -0400

    Added spec for post images admin interface.

--- spec/factories/post_image.rb
+++ spec/factories/post_image.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :post_image, class: Spree::PostImage do
     association :viewable, factory: :post
     attachment { File.new(File.expand_path("../../../spec/factories/1.jpg", __FILE__)) }
+    alt { Faker::Lorem.words.join ' ' }
   end
 end
--- spec/features/admin/posts_spec.rb
+++ spec/features/admin/posts_spec.rb
@@ -34,6 +34,34 @@ feature "Blog Posts", js: true do
     then_i_should_see_it_removed_in_the_list
   end
 
+  scenario "User can see images for a blog post" do
+    given_i_sign_in
+    and_i_have_posts_with_images
+    when_i_view_post_images
+    then_i_should_see_the_images_for_the_post
+  end
+
+  scenario "User can add images to a blog post" do
+    given_i_sign_in
+    and_i_have_posts
+    when_i_add_an_image
+    then_i_should_see_the_image_on_my_post
+  end
+
+  scenario "User can edit images on a blog post" do
+    given_i_sign_in
+    and_i_have_posts_with_images
+    when_i_edit_an_image
+    then_i_should_see_the_updated_image_on_my_post
+  end
+
+  scenario "User can delete images on a blog post" do
+    given_i_sign_in
+    and_i_have_posts_with_images
+    when_i_delete_an_image
+    then_i_should_see_the_image_removed_from_my_post
+  end
+
   def given_i_sign_in
     visit spree.login_path
     within '#new_spree_user' do
@@ -50,11 +78,27 @@ feature "Blog Posts", js: true do
     @post_b = FactoryGirl.create(:post)
   end
 
+  def and_i_have_posts_with_images
+    and_i_have_posts
+    @post_a_image_1 = FactoryGirl.create(:post_image, viewable: @post_a)
+    @post_a_image_2 = FactoryGirl.create(:post_image, viewable: @post_a)
+    @post_b_image_1 = FactoryGirl.create(:post_image, viewable: @post_b)
+  end
+
   def when_i_visit_admin_posts
     find('.contents-nav').find('a').click
     expect(page).to have_current_path(spree.admin_pages_path)
     click_link 'Blog Posts'
     expect(page).to have_current_path(spree.admin_posts_path)
+  end
+
+  def when_i_view_post_images
+    when_i_visit_admin_posts
+    within "#post_#{@post_a.id}" do
+      find("a.edit").click
+    end
+    find('.post-images').click
+    expect(page).to have_current_path(spree.admin_post_images_path(@post_a))
   end
 
   def and_i_create_a_post
@@ -88,6 +132,37 @@ feature "Blog Posts", js: true do
     alert.accept
   end
 
+  def when_i_add_an_image
+    when_i_view_post_images
+    click_link "New Image"
+    attach_file("Attachment", File.expand_path('../../factories/1.jpg', File.dirname(__FILE__)))
+    fill_in "post_image_alt", with: "Test Image"
+    click_button "Update"
+  end
+
+  def when_i_edit_an_image
+    when_i_view_post_images
+    within "#post_image_#{@post_a_image_1.id}" do
+      find("a.edit").click
+    end
+
+    @old_title = @post_a_image_1.alt
+    fill_in "post_image_alt", with: "Test Image Update"
+    attach_file("Attachment", File.expand_path('../../factories/1.png', File.dirname(__FILE__)))
+    click_button "Update"
+  end
+
+  def when_i_delete_an_image
+    when_i_view_post_images
+    @old_title = @post_a_image_1.alt
+    within "#post_image_#{@post_a_image_1.id}" do
+      find("a.delete-resource").click
+    end
+    wait = Selenium::WebDriver::Wait.new ignore: Selenium::WebDriver::Error::NoAlertPresentError
+    alert = wait.until { page.driver.browser.switch_to.alert }
+    alert.accept
+  end
+
   def then_i_should_see_my_posts
     expect(page).to have_content(@post_a.title)
     expect(page).to have_content(@post_b.title)
@@ -110,6 +185,31 @@ feature "Blog Posts", js: true do
   def then_i_should_see_it_removed_in_the_list
     expect(page).to have_current_path(spree.admin_posts_path)
     expect(page).to have_content("Post \"#{@old_title}\" has been successfully removed!")
+    expect(page).to_not have_selector('td', text: /#{@old_title}/i)
+  end
+
+  def then_i_should_see_the_images_for_the_post
+    expect(page).to have_content(@post_a_image_1.alt)
+    expect(page).to have_content(@post_a_image_2.alt)
+    expect(page).to_not have_content(@post_b_image_1.alt)
+  end
+
+  def then_i_should_see_the_image_on_my_post
+    expect(page).to have_current_path(spree.admin_post_images_path(@post_a))
+    expect(page).to have_content("Post image has been successfully created!")
+    expect(page).to have_selector('td', text: /Test Image/i)
+  end
+
+  def then_i_should_see_the_updated_image_on_my_post
+    expect(page).to have_current_path(spree.admin_post_images_path(@post_a))
+    expect(page).to have_content("Post image has been successfully updated!")
+    expect(page).to have_content("Test Image Update")
+    expect(page).to_not have_selector('td', text: /#{@old_title}/i)
+  end
+
+  def then_i_should_see_the_image_removed_from_my_post
+    expect(page).to have_current_path(spree.admin_post_images_path(@post_a))
+    expect(page).to have_content("Post image has been successfully removed!")
     expect(page).to_not have_selector('td', text: /#{@old_title}/i)
   end
 end
